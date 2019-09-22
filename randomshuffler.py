5ea34c34fbd7ed6acee60e0675d4c8bd77181585
Bishwas Mandal <bishwasmandal246@gmail.com>
2019-09-22 23:17:58 +0530

    Add files via upload

---
+++ randomshuffler.py
@@ -0,0 +1,6 @@
+#Preparing for GRE vocab can be a daunting task specially if we start memorizing word sequentially. Because that gives you a misconception that you know the word's meaning however you don't when the sequence is changed. So try revising words you know by generating this list and you can actually know whether you really know a word's meaning or not
+
+import random
+A=['word1','word2','word3','so_on']
+random.shuffle(A)
+print(A)
